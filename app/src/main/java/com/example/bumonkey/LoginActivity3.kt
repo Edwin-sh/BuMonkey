8d31818369ca32d952f8a10b1ee11cfb02d852b3
DANIELLEONARDORODRIGUEZ <leonardo.rodriguez.k@gmail.com>
2022-10-23 19:47:37 -0500

    actividad 3 login

---
+++ app/src/main/java/com/example/bumonkey/LoginActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.bumonkey
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login3)
+    }
+}
