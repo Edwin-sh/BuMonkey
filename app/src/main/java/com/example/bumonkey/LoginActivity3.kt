29648367304123cc1c0aaf41cfb18431f6736e36
DANIELLEONARDORODRIGUEZ <leonardo.rodriguez.k@gmail.com>
2022-10-24 14:51:20 -0500

    .

--- app/src/main/java/com/example/bumonkey/LoginActivity3.kt
+++ app/src/main/java/com/example/bumonkey/LoginActivity3.kt
@@ -2,10 +2,13 @@ package com.example.bumonkey
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class LoginActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login3)
+
     }
 }
