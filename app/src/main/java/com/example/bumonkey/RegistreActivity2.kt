02825dcfac78426d8a531f7534b0c6779d2a5c0c
DANIELLEONARDORODRIGUEZ <leonardo.rodriguez.k@gmail.com>
2022-10-27 00:09:10 -0500

    DETALLES HABLADOS CODIGO

--- app/src/main/java/com/example/bumonkey/RegistreActivity2.kt
+++ app/src/main/java/com/example/bumonkey/RegistreActivity2.kt
@@ -1,11 +1,21 @@
 package com.example.bumonkey
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegistreActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registre2)
+    }
+    fun home(buttoncancel: View) {
+        val cancel = Intent(this, MainActivity::class.java)
+        startActivity(cancel)
+    }
+    fun login(buttonlogin: View){
+        val login= Intent(this,LoginActivity3::class.java)
+        startActivity(login)
     }
 }
