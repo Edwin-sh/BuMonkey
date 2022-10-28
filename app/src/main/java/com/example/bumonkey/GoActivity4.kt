c27d7ec18398da98ed1609322cf818da70510986
DANIELLEONARDORODRIGUEZ <leonardo.rodriguez.k@gmail.com>
2022-10-28 18:50:23 -0500

    menu barra

--- app/src/main/java/com/example/bumonkey/GoActivity4.kt
+++ app/src/main/java/com/example/bumonkey/GoActivity4.kt
@@ -2,6 +2,7 @@ package com.example.bumonkey
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class GoActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +10,10 @@ class GoActivity4 : AppCompatActivity() {
         setContentView(R.layout.activity_go4)
         setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
