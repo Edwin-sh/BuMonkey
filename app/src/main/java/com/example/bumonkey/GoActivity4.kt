5068534dc3308c602d127e9a7c1cf20d2b6da520
DANIELLEONARDORODRIGUEZ <leonardo.rodriguez.k@gmail.com>
2022-10-29 10:35:05 -0500

    Revert "Merge branch 'Edwin'"
    This reverts commit dbcce4b95d83e3fbc8bc6fc835e18b0a8a90f2b7, reversing
    changes made to 784fc0443fd93d9b8eaa365617f7853bf97a69b9.

--- app/src/main/java/com/example/bumonkey/GoActivity4.kt
+++ app/src/main/java/com/example/bumonkey/GoActivity4.kt
@@ -1,12 +1,97 @@
 package com.example.bumonkey
 
-import android.support.v7.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 class GoActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_go4)
-        setSupportActionBar(findViewById(R.id.header))
+        setSupportActionBar(findViewById(R.id.toolbar))
+        this.llamargastos(null)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean=when(item.itemId) {
+
+        R.id.help->{
+            val positiveButton={ dialog: DialogInterface, entero:Int-> }
+
+            val alert = AlertDialog.Builder(this).setTitle(R.string.Alert_dialog_tittle_help)
+                .setMessage(R.string.Alert_dialog_message_help)
+                .setPositiveButton("OK",positiveButton)
+                .create().show()
+            true
+        }
+
+        R.id.search->{
+            Toast.makeText(this,R.string.Toast_search,Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.edit->{
+            Toast.makeText(this,R.string.Toast_edit,Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.delete->{
+            Toast.makeText(this,R.string.Toast_delete,Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.email->{
+            Toast.makeText(this,R.string.Toast_email,Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.upload->{
+            Toast.makeText(this,R.string.Toast_upload,Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.exit->{
+            Toast.makeText(this,R.string.Alert_dialog_tittle_exit,Toast.LENGTH_LONG).show()
+            val exit= Intent(this,MainActivity::class.java)
+            startActivity(exit)
+            Toast.makeText(this,R.string.Alert_dialog_message_exit, Toast.LENGTH_LONG).show()
+            true
+        }
+        else->{
+            super.onOptionsItemSelected(item)
+        }
+
+       // return super.onOptionsItemSelected(item)
+    }
+
+    fun llamargastos(view: View?){
+        if (view!=null){
+            Toast.makeText(this, getString(R.string.Toast_expenses), Toast.LENGTH_SHORT).show()
+        }
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.fragcontainer, gastos_fragment::class.java, null, "tarea")
+            .commit()
+    }
+
+    fun llamaringresos(view: View?){
+        if (view!=null){
+            Toast.makeText(this, getString(R.string.Toast_earnings), Toast.LENGTH_SHORT).show()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.fragcontainer, ingresos_fragment::class.java, null, "tarea2")
+            .commit()
     }
 }
--- app/src/main/java/com/example/bumonkey/LoginActivity3.kt
+++ app/src/main/java/com/example/bumonkey/LoginActivity3.kt
@@ -1,26 +1,58 @@
 package com.example.bumonkey
 
+import android.content.DialogInterface
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class LoginActivity3 : AppCompatActivity() {
+    private var edtuser:EditText?=null
+    private var edtpassword:EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login3)
-        setSupportActionBar(findViewById(R.id.header))
+
+        edtuser=findViewById(R.id.editTextTextEmailAddress)
+        edtpassword=findViewById(R.id.editTextTextPassword)
     }
 
     fun home(buttoncancel:View){
         val cancel= Intent(this,MainActivity::class.java)
         startActivity(cancel)
+        Toast.makeText(this,R.string.Toast_butt_cancel, Toast.LENGTH_LONG).show()
     }
 
-    fun go(buttonlogin:View){
-        val login= Intent(this,GoActivity4::class.java)
-        startActivity(login)
+    fun go(buttonloging:View) {
+        var username: String = edtuser!!.text.toString()
+        var password: String = edtpassword!!.text.toString()
+
+        val positiveButton={ dialog:DialogInterface,entero:Int->
+            val login = Intent(this, GoActivity4::class.java)
+            startActivity(login)
+            Toast.makeText(this,R.string.Toast_butt_pos,Toast.LENGTH_LONG).show()
+
+        }
+        val negativeButton={ dialog:DialogInterface,entero:Int->
+            Toast.makeText(this,R.string.Toast_butt_neg,Toast.LENGTH_LONG).show()
+        }
+
+        if (username == "grupo3@bumonkey.com" && password == "bumonkeyGP3") {
+            val alert = AlertDialog.Builder(this).setTitle(R.string.Alert_dialog_tittle_pos)
+                .setMessage(R.string.Alert_dialog_message_pos)
+                .setPositiveButton(R.string.possitive_button,positiveButton)
+                .setNegativeButton(R.string.negative_button,negativeButton)
+                .create().show()
+
+        } else {
+            val alert = AlertDialog.Builder(this).setTitle(R.string.Alert_dialog_tittle_neg)
+                .setMessage(R.string.Alert_dialog_message_neg).create().show()
+        }
     }
+
 
 }
